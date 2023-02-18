8e7d4eb3177a4c4cf840fa7f87e60717eb2cb79d
MuanyaStella <chimuanyastella@gmail.com>
2023-02-18 17:55:37 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- alphabets in lowercase
+ * Return: 0 successful
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
