2c34a495046906c4f5278c86a2dcea1f1f6e5d73
MuanyaStella <chimuanyastella@gmail.com>
2023-02-18 19:02:20 +0000

    print base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - digits of base 10
+ * Return: 0 successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; 0 < 10 ; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
