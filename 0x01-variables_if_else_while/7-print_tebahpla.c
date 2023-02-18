1dcfa4ea768b6bd5e76f653213f2baef02e895b7
MuanyaStella <chimuanyastella@gmail.com>
2023-02-18 20:39:49 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in reverse
+ * Return: 0 successful
+ */
+int main(void)
+{
+	char(x);
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
