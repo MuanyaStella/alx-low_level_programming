41a23abbddc26fcfacec829989c74303339db64e
MuanyaStella <chimuanyastella@gmail.com>
2023-02-19 22:21:15 +0000

    three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of three digits
+ * Return: 0 successful
+ */
+int main(void)
+{
+	int k;
+	int l;
+	int m;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (l = 1; l < 10; l++)
+		{
+			for (m = 2; m < 10; m++)
+			{
+				if (k < l && l < m)
+				{
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(m + '0');
+					if (k + l + m != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
