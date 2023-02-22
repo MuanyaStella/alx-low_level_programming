f26a86a9c9de58d2e5f1221f6536093f68e02611
MuanyaStella <chimuanyastella@gmail.com>
2023-02-22 21:10:20 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - mai function
+ * print_alphabet: prints alphabets in lower case
+ * Return: 0 successful
+ */
+
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		_putchar(x);
+	_putchar('\n');
+}
