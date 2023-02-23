0f5e118bd665174d80a284103b846543508b085a
MuanyaStella <chimuanyastella@gmail.com>
2023-02-23 00:52:17 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @x: parameter 1
+ * @y: parameter 2
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
