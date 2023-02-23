1972f1bc5d507fb3dc0fd82aa1bd84cf8320a1a3
MuanyaStella <chimuanyastella@gmail.com>
2023-02-23 04:53:32 +0000

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,41 +8,24 @@
  * Return: no return
  */
 
-void print_times_table(int n)
+void times_table(void)
 {
-	int a, b, op;
+	int i, mult, prod;
 
-	if (n >= 0 && n <= 15)
+	for (i = 0; i <= 9; i++)
 	{
-		for (a = 0; a <= n; a++)
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
+			_putchar('.');
+			_putchar(' ');
+			prod = i * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
-
