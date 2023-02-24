4391aa1687a2a104b003167d383c8f75cc868d27
MuanyaStella <chimuanyastella@gmail.com>
2023-02-24 22:11:32 +0000

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - pribts a triangle
+ *
+ * @size: size of triangle
+ *
+ * Return: 0 successful
+ */
+
+void print_triangle(int size)
+{
+
+	int l;
+	int t;
+
+	if (size > 0)
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (t =size - l; t > 0; t--)
+				_putchar(' ');
+			for (t = 0; t < l; t++)
+				_putchar('#');
+			if (l == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
