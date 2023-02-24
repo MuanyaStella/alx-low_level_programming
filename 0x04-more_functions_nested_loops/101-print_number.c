8a131cbe457689146f4485ade6232cc0043474ae
MuanyaStella <chimuanyastella@gmail.com>
2023-02-24 23:52:55 +0000

    num

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  print_number - function prints an integer
+ *
+ * @n: interger to print
+ *
+ * Return: 0 successful
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar(x + '0');
+}
