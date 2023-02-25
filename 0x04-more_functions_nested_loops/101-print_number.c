b67bf10dfa7c64eb49f7cf467eaea43dd4d42292
MuanyaStella <chimuanyastella@gmail.com>
2023-02-25 00:05:02 +0000

    num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,14 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		x = -x;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
