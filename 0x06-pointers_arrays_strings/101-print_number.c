a6cf938779a98a6ab1f4c5d5495dad259dc3280a
MuanyaStella <chimuanyastella@gmail.com>
2023-03-03 03:18:32 +0000

    integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer
+ */
+
+void print_number(int n)
+{
+	unsigned int y = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		y = -y;
+	}
+	if ((y / 10) > 0)
+		print_number(y / 10);
+	_putchar((y % 10) + '0');
+}
