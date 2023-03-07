cff840deb46f7092f2a2e83632431868a539d1b4
MuanyaStella <chimuanyastella@gmail.com>
2023-03-07 11:21:12 +0000

    locates character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates the character of a string.
+ * @s: the string.
+ * @c: character to be located.
+ *
+ * Return: pointer to the fisrt occurence if c is found.
+ * Null, if c is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\n'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
