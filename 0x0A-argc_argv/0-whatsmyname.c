8d96539561a86ac0be80ff8fb95886adfb1c4640
MuanyaStella <chimuanyastella@gmail.com>
2023-03-11 04:30:24 +0000

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - writes a program that prints its name followed by a new line.
+ * @argc: Argument count
+ * @argv: Arguement vector
+ *
+ * Return: 0 successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
