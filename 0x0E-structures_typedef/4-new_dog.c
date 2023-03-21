4d780805bdca9ccdfcf1a8b0087b1602d6b811bd
MuanyaStella <chimuanyastella@gmail.com>
2023-03-21 23:28:52 +0000

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ char *_strcpy(char *dest, char *src)
 
 {
 	int len, i;
-	
+
 	len = 0;
 	while (src[len] != '\0')
 	{
@@ -65,9 +65,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 {
 	dog_t *dog;
-	
+
 	int len1, len2;
-	
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
