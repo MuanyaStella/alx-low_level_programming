92b547ba121e67d7296bf4933f63b5f21fe859e6
MuanyaStella <chimuanyastella@gmail.com>
2023-04-09 16:13:50 +0000

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 {
 	unsigned long int nbits;
 
-	for (nbits = 0; || m; n >>= 1, m >>=1)
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			nbits++;
