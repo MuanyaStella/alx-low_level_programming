eaaf9b7ac4423cbeca6f3db0aa003701733662c7
MuanyaStella <chimuanyastella@gmail.com>
2023-04-11 20:57:06 +0000

    crackme3

---
+++ 0x14-bit_manipulation/password.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *fp;
+	char filename[] = "Hol\4";
+
+	/* Open the file for writing */
+	fp = fopen("101-password", "w");
+
+	/* Check if the file was successfully opened */
+	if (fp == NULL)
+	{
+		printf("Failed to open file: %s\n", filename);
+		return (1);
+	}
+
+	/* Write some data to the file */
+	fputs(filename, fp);
+
+	/* Close the file */
+	fclose(fp);
+
+	return (0);
+}
